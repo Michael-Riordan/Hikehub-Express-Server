2d1c7555918a9a3a8fc238007e02b87c7ec9a9a6
Michael <michael.riordan81@outlook.com>
2023-09-01 08:03:16 -0700

    initialize hikehub-server

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+const port = 3000;
+app.use(cors());
+
+app.get('/api/activities', async (req, res) => {
+    const API_KEY = process.env.RECDOTGOV_API_KEY;
+    try {
+        const response = await fetch(`https://ridb.recreation.gov/api/v1/activities?limit=50&offset=0&apikey=${API_KEY}`);
+        const jsonResponse = await response.json();
+
+        res.send(jsonResponse);
+    } catch (error) {
+        console.error('Error fetching activity data', error);
+        res.status(500).json({ error: 'Internal server error'});
+    }
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`)
+});
