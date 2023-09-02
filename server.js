58eb1b02fce3094764693a1984c324f1c82d3bfd
Michael <michael.riordan81@outlook.com>
2023-09-02 09:57:07 -0700

    add offset parameter to activities api call

--- server.js
+++ server.js
@@ -8,8 +8,10 @@ app.use(cors());
 
 app.get('/api/activities', async (req, res) => {
     const API_KEY = process.env.RECDOTGOV_API_KEY;
+    const offset = req.query.offset || 0;
+
     try {
-        const response = await fetch(`https://ridb.recreation.gov/api/v1/activities?limit=50&offset=0&apikey=${API_KEY}`);
+        const response = await fetch(`https://ridb.recreation.gov/api/v1/activities?limit=50&offset=${offset}&apikey=${API_KEY}`);
         const jsonResponse = await response.json();
 
         res.send(jsonResponse);
