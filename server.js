ea30fa9e4facd9496c6efcde3824f342d5ae51b9
Michael <michael.riordan81@outlook.com>
2023-09-06 15:49:40 -0700

    add get method for National Parks and Rec Area Images

--- server.js
+++ server.js
@@ -6,6 +6,24 @@ const app = express();
 const port = 3000;
 app.use(cors());
 
+app.get('/api/geolocation', async (req, res) => {
+    const API_KEY = process.env.GOOGLE_MAPS_API_KEY;
+    const coordinates = req.query.coordinates;
+    const coordinatesObject = JSON.parse(decodeURIComponent(coordinates));
+    const latitude = coordinatesObject.latitude;
+    const longitude = coordinatesObject.longitude;
+
+    try {
+        const response = await fetch(`https://maps.googleapis.com/maps/api/geocode/json?latlng=${latitude},${longitude}&key=${API_KEY}`);
+        const jsonResponse = await response.json();
+
+        res.send(jsonResponse);
+    } catch (error) {
+        console.error('Error fetching location', error);
+        res.status(500).json({ error: 'Internal server error'});
+    }
+});
+
 app.get('/api/activities', async (req, res) => {
     const API_KEY = process.env.RECDOTGOV_API_KEY;
     const offset = req.query.offset || 0;
@@ -35,27 +53,39 @@ app.get('/api/recAreas', async (req, res) => {
         res.send(jsonResponse);
     } catch (error) {
         console.error('Error fetching recArea data', error);
-        res.status(500).json({ error: 'Internal server error.'})
+        res.status(500).json({ error: 'Internal server error.'});
     }
 })
 
-app.get('/api/geolocation', async (req, res) => {
-    const API_KEY = process.env.GOOGLE_MAPS_API_KEY;
-    const coordinates = req.query.coordinates;
-    const coordinatesObject = JSON.parse(decodeURIComponent(coordinates));
-    const latitude = coordinatesObject.latitude;
-    const longitude = coordinatesObject.longitude;
+app.get('/api/recAreaImg', async (req, res) => {
+    const API_KEY = process.env.RECDOTGOV_API_KEY;
+    const idQuery = req.query.id;
 
     try {
-        const response = await fetch(`https://maps.googleapis.com/maps/api/geocode/json?latlng=${latitude},${longitude}&key=${API_KEY}`);
+        const response = await fetch (`https://ridb.recreation.gov/api/v1/recareas/${idQuery}/media?limit=50&offset=0&apikey=${API_KEY}`)
         const jsonResponse = await response.json();
 
         res.send(jsonResponse);
     } catch (error) {
-        console.error('Error fetching location', error);
-        res.status(500).json({ error: 'Internal server error'});
+        console.error('Error fetching Rec Area Images', error);
+        res.status(500).json( {error: 'Internal server error.'});
     }
-});
+})
+
+app.get('/api/NationalParks', async (req, res) => {
+    const API_KEY = process.env.DATADOTGOV_API_KEY;
+    const stateCode = req.query.stateCode;
+
+    try {
+        const response = await fetch(`https://developer.nps.gov/api/v1/parks?stateCode=${stateCode}&api_key=${API_KEY}`)
+        const jsonResponse = await response.json();
+        res.send(jsonResponse);
+    } catch (error) {
+        console.error('Error fetching National Parks data', error);
+        res.status(500).json({ error: 'Internal server error.'});
+    }
+})
+
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`)
