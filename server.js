27a0cca6fef0a592169174e2595dd060cef0c908
Michael <michael.riordan81@outlook.com>
2023-09-26 09:51:47 -0700

    add fetch request to NatPark.gov API for things to do by user selected park

--- server.js
+++ server.js
@@ -125,6 +125,20 @@ app.get('/api/NationalParkGeoJson', async (req, res) => {
     }
 })
 
+app.get('/api/NatParkThingsToDo', async (req, res) => {
+    const API_KEY = process.env.DATADOTGOV_API_KEY;
+    const parkCode = req.query.parkCode;
+
+    try {
+        const response = await fetch(`https://developer.nps.gov/api/v1/thingstodo?parkCode=${parkCode}&api_key=${API_KEY}`)
+        const jsonResponse = await response.json();
+        res.send(jsonResponse);
+    } catch (error) {
+        console.error(`Error fetching Things to Do in ${parkCode}`);
+        res.status(500).json({ error: 'Internal server error.' });
+    }
+})
+
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`)
